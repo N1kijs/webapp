d903512160bcf35064fabcf3adcccff70ca6e5b2
N1kijs <48949270+N1kijs@users.noreply.github.com>
2019-12-10 08:56:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
