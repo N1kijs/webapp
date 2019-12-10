d2d5b89cb2c5aaaa45297cd7d26c73a2bf660853
N1kijs <48949270+N1kijs@users.noreply.github.com>
2019-12-10 09:58:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,14 @@ def cakes():
 
 @app.route('/hello/<name>')
 def hello(name):
+    if name == Nikij
+        GPIO.output(17,GPIO.HIGH)
+        GPIO.output(27,GPIO.HIGH)
+        time.sleep(1)
+
+
     return render_template('page.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
